73cdc2e64c3aafbc84b185aea4d6358635d69098
MEXSlacker <46721097+MEXSlacker@users.noreply.github.com>
2020-01-15 15:55:29 -0600

    Add files via upload

---
+++ react tutorial/act2/act 2/practice 1/my-app/build/precache-manifest.2075e896b5ad3dd53ba211efce23a177.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f2778a1bf9f9d54f8ba975296bec71d0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fde72324bf35cc556f8c",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "706f41ffbdf26bf106bf",
+    "url": "/static/js/2.39513104.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.39513104.chunk.js.LICENSE"
+  },
+  {
+    "revision": "fde72324bf35cc556f8c",
+    "url": "/static/js/main.e335d515.chunk.js"
+  },
+  {
+    "revision": "177d52efa2c7db582a05",
+    "url": "/static/js/runtime-main.5b50bb1e.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
